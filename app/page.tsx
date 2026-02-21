4ab2f74b40b15ead8dffa0510f2f6808ae533aab
Kzlyr <kmanejo@up.edu.ph>
2026-02-22 02:16:35 +0800

    feat: add animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,48 @@
+// import "../styles/teaser.css";
+
+// export default function Home() {
+//   return (
+//     <main className="teaser">
+//       <div className="teaser-content">
+//         <h1>Coming Soon.</h1>
+//         <p>We’re building something great.</p>
+//         <p>
+//           For now, visit:{" "}
+//           <a href="https://kmanejo.wixstudio.com/trlars" target="_blank">TRACER</a>
+//         </p>
+//       </div>
+//     </main>
+//   );
+// }
+
 import "../styles/teaser.css";
 
 export default function Home() {
   return (
     <main className="teaser">
+
+      {/* Background bubbles */}
+      <div className="bubbles">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
       <div className="teaser-content">
         <h1>Coming Soon.</h1>
         <p>We’re building something great.</p>
+
         <p>
           For now, visit:{" "}
-          <a href="https://kmanejo.wixstudio.com/trlars" target="_blank">TRACER</a>
+          <a href="https://kmanejo.wixstudio.com/trlars" target="_blank">
+            TRACER
+          </a>
         </p>
       </div>
+
     </main>
   );
 }
