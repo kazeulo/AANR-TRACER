bd33481f8ecf28aa83fabb3730082ecf0bdd0e6a
Kzlyr <kmanejo@up.edu.ph>
2026-05-02 20:48:10 +0800

    fix: faqUtils directory in faqs page imports

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useRef } from "react";
-import { categories } from "../../lib/faqUtils";
+import { categories } from "@/data/faqUtils";
 
 // SVG Icon component 
 
